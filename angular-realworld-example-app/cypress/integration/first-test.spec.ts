062576b3ea21a0416792ea4e7da655154816ca95
Milton Velilla <milton.velilla@overactive.com>
2022-01-05 14:33:11 -0500

    add test

--- angular-realworld-example-app/cypress/integration/first-test.spec.ts
+++ angular-realworld-example-app/cypress/integration/first-test.spec.ts
@@ -2,7 +2,7 @@
 
 describe('Test with backend', () => {
     beforeEach(() => {
-        cy.intercept('GET', '**/tags', {fixture: 'tags.json'});        
+        cy.intercept({ method: 'GET', path: 'tags' }, { fixture: 'tags.json' });
         (cy as any).loginToApplication();
     });
 
@@ -10,21 +10,44 @@ describe('Test with backend', () => {
 
         cy.intercept('POST', '**/articles').as('postArticles');
 
-        cy.intercept({
-            method: 'POST',
-            url: '/articles',
-            hostname: 'localhost:4200'
-        }).as('postArticles');
-
         cy.log('we logged in!!');
         cy.contains('New Article').click();
-        cy.get('[formcontrolname="title"]').type('This is a title 97');
+        cy.get('[formcontrolname="title"]').type('This is a title 94');
         cy.get('[formcontrolname="description"]').type('This is a description');
         cy.get('[formcontrolname="body"]').type('This is a body of the article');
         cy.contains('Publish Article').click();
 
         cy.wait('@postArticles');
         cy.get('@postArticles').then(xhr => console.log(xhr))
+    });
+
+    it(`modifying the request`, () => {
+
+        const descTwo = 'Description two';
+
+        cy.intercept('POST', '**/articles', req => {
+            req.body.article.description = 'My description intercepted';
+        }).as('postArticles');
+
+        // cy.intercept('POST', '**/articles', req => {
+        //     req.reply(res => {
+        //         console.log('res...', res)
+        //         expect(res.body.article.description).to.equal('This is a description');
+        //         res.body.article.description = descTwo;
+        //     });            
+        // }).as('postArticles');
+
+        cy.contains('New Article').click();
+        cy.get('[formcontrolname="title"]').type('This is a title 91');
+        cy.get('[formcontrolname="description"]').type('This is a description');
+        cy.get('[formcontrolname="body"]').type('This is a body of the article');
+        cy.contains('Publish Article').click();
+
+        cy.wait('@postArticles');
+        cy.get('@postArticles').then((xhr: any) => {
+            console.log(xhr);
+            // expect(xhr.response.body.article.description).to.equal(descTwo);
+        });
     });
 
     it(`verify correct request and response`, () => {
@@ -34,7 +57,7 @@ describe('Test with backend', () => {
             .and('contain', 'testing');
     });
 
-    it.only(`verify global articles`, () => {
+    it(`verify global articles`, () => {
         cy.intercept('GET', '**/articles/feed*', '{"articles":[],"articlesCount":0}');
         cy.intercept('GET', '**/articles*', { fixture: 'articles.json' });
 
@@ -50,5 +73,56 @@ describe('Test with backend', () => {
         });
 
         cy.get('app-article-list button').eq(1).click();
+    });
+
+    it.only(`Delete a new article`, () => {
+        const articleTitle = `Article ${new Date().getTime()}`;
+        const bodyRequest = {
+            "article": {
+                "tagList": [
+                    "angular",
+                    "testing"
+                ],
+                "title": articleTitle,
+                "description": "sub title",
+                "body": "description"
+            }
+        };
+
+        cy.get('@token').then(token => {
+            cy.request({
+                url: 'https://api.realworld.io/api/articles/',
+                headers: {
+                    'Authorization': `Token ${token}`
+                },
+                method: 'POST',
+                body: bodyRequest
+            }).then(response => {
+                expect(response.status).to.equal(200);
+            });
+
+            cy.contains('Global Feed').click();
+            cy.get('a h1').contains(articleTitle).click();
+            cy.get('.article-actions').contains('Delete Article').click();
+
+            cy.wait(500);
+
+            cy.request({
+                url: 'https://api.realworld.io/api/articles?limit=10&offset=0',
+                headers: {
+                    'Authorization': `Token ${token}`
+                },
+                method: 'GET',
+            }).its('body').then(body => {
+                const articles = body.articles;
+                expect(articles.length).to.greaterThan(0);
+                console.log(articles);
+                for (const art of articles) {
+                    console.log('article name', articleTitle);
+                    expect(art.title).not.to.equal(articleTitle);
+                }
+            });
+        });
+
     });
 });
--- angular-realworld-example-app/cypress/support/commands.js
+++ angular-realworld-example-app/cypress/support/commands.js
@@ -24,9 +24,31 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('loginToApplication', () => {
-    cy.visit("/login");
-    cy.get('[placeholder="Email"]').type('artem.bondar16@gmail.com');
-    cy.get('[placeholder="Password"]').type('CypressTest1');
-    cy.get('form').submit();
+Cypress.Commands.add("loginToApplication", () => {
+  const userCredentials = {
+    user: {
+      email: "artem.bondar16@gmail.com",
+      password: "CypressTest1",
+    },
+  };
+  cy.request(
+    "POST",
+    "https://api.realworld.io/api/users/login",
+    userCredentials
+  )
+    .its("body")
+    .then((body) => {
+      const token = body.user.token;
+      cy.wrap(token).as('token');
+      cy.visit("/", {          
+        onBeforeLoad(win) {
+          win.localStorage.setItem("jwtToken", token);
+        },
+      });
+    });
+
+  //   cy.visit("/login");
+  //   cy.get('[placeholder="Email"]').type("artem.bondar16@gmail.com");
+  //   cy.get('[placeholder="Password"]').type("CypressTest1");
+  //   cy.get("form").submit();
 });
